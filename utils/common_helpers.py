d48493df1698feab5f1f3e0c7f3ddbd8196d67ba
Falaah Arif Khan <falaaharifkhan@10-19-83-0.dynapool.wireless.nyu.edu>
2022-12-06 21:42:54 -0500

    Added generic classes

---
+++ utils/common_helpers.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+from sklearn.metrics import confusion_matrix
+
+def partition_by_group_intersectional(df, column_names, priv_values):
+    priv = df[(df[column_names[0]] == priv_values[0]) & (df[column_names[1]] == priv_values[1])]
+    dis = df[(df[column_names[0]] != priv_values[0]) & (df[column_names[1]] != priv_values[1])]
+    return priv, dis
+
+def partition_by_group_binary(df, column_name, priv_value):
+    priv = df[df[column_name] == priv_value]
+    dis = df[df[column_name] != priv_value]
+    if len(priv)+len(dis) != len(df):
+        raise ValueError("Error! Not a partition")
+    return priv, dis
+
+def set_protected_groups(X_test, column_names, priv_values):
+    groups={}
+    groups[column_names[0]+'_'+column_names[1]+'_priv'], groups[column_names[0]+'_'+column_names[1]+'_dis'] = partition_by_group_intersectional(X_test, column_names, priv_values)
+    groups[column_names[0]+'_priv'], groups[column_names[0]+'_dis'] =  partition_by_group_binary(X_test, column_names[0], priv_values[0])
+    groups[column_names[1]+'_priv'], groups[column_names[1]+'_dis'] =  partition_by_group_binary(X_test, column_names[1], priv_values[1])
+    return groups
+
+def confusion_matrix_metrics(y_true, y_preds):
+    metrics={}
+    TN, FP, FN, TP = confusion_matrix(y_true, y_preds).ravel()
+    metrics['TPR'] = TP/(TP+FN)
+    metrics['TNR'] = TN/(TN+FP)
+    metrics['PPV'] = TP/(TP+FP)
+    metrics['FNR'] = FN/(FN+TP)
+    metrics['FPR'] = FP/(FP+TN)
+    metrics['Accuracy'] = (TP+TN)/(TP+TN+FP+FN)
+    metrics['F1'] = (2*TP)/(2*TP+FP+FN)
+    metrics['Selection-Rate'] = (TP+FP)/(TP+FP+TN+FN)
+    metrics['Positive-Rate'] = (TP+FP)/(TP+FN) 
+    return metrics
---
+++ utils/data_loader.py
@@ -0,0 +1,229 @@
+import pandas as pd
+import numpy as np
+from sys import getsizeof
+from folktables import ACSDataSource, ACSEmployment, ACSIncome, ACSTravelTime, ACSPublicCoverage, ACSMobility
+
+class ACSMobilityDataset():
+    def __init__(self, state, year, with_nulls=False):
+        data_source = ACSDataSource(
+            survey_year=year,
+            horizon='1-Year',
+            survey='person'
+        )
+        acs_data = data_source.get_data(states=state, download=True)
+        self.features = ACSMobility.features
+        self.target = ACSMobility.target
+        self.categorical_columns = ['MAR','SEX','DIS','ESP','CIT','MIL','ANC','NATIVITY','RELP','DEAR','DEYE','DREM','RAC1P','GCL','COW','ESR']
+        self.numerical_columns = ['AGEP', 'SCHL', 'PINCP', 'WKHP', 'JWMNP']
+
+        if with_nulls==True:
+            X_data = acs_data[self.features]
+        else:
+            X_data = acs_data[self.features].apply(lambda x: np.nan_to_num(x, -1))
+
+        self.X_data = X_data[self.categorical_columns].astype('str')
+        for col in self.numerical_columns:
+            self.X_data[col] = X_data[col]
+            
+        self.y_data = acs_data[self.target].apply(lambda x: int(x == 1))
+
+        self.columns_with_nulls = self.X_data.columns[self.X_data.isna().any().to_list()].to_list()
+
+    def update_X_data(self, X_data):
+        '''
+        To save simulated nulls
+        '''
+        self.X_data = X_data
+        self.columns_with_nulls = self.X_data.columns[self.X_data.isna().any().to_list()].to_list()
+
+class ACSPublicCoverageDataset():
+    def __init__(self, state, year, with_nulls=False):
+        data_source = ACSDataSource(
+            survey_year=year,
+            horizon='1-Year',
+            survey='person'
+        )
+        acs_data = data_source.get_data(states=state, download=True)
+        self.features = ACSPublicCoverage.features
+        self.target = ACSPublicCoverage.target
+        self.categorical_columns = ['MAR','SEX','DIS','ESP','CIT','MIG','MIL','ANC','NATIVITY','DEAR','DEYE','DREM','ESR','ST','FER','RAC1P']
+        self.numerical_columns = ['AGEP', 'SCHL', 'PINCP']
+
+        if with_nulls==True:
+            X_data = acs_data[self.features]
+        else:
+            X_data = acs_data[self.features].apply(lambda x: np.nan_to_num(x, -1))
+
+        self.X_data = X_data[self.categorical_columns].astype('str')
+        for col in self.numerical_columns:
+            self.X_data[col] = X_data[col]
+            
+        self.y_data = acs_data[self.target].apply(lambda x: int(x == 1))
+
+        self.columns_with_nulls = self.X_data.columns[self.X_data.isna().any().to_list()].to_list()
+
+    def update_X_data(self, X_data):
+        '''
+        To save simulated nulls
+        '''
+        self.X_data = X_data
+        self.columns_with_nulls = self.X_data.columns[self.X_data.isna().any().to_list()].to_list()
+
+class ACSTravelTimeDataset():
+    def __init__(self, state, year, with_nulls=False):
+        data_source = ACSDataSource(
+            survey_year=year,
+            horizon='1-Year',
+            survey='person'
+        )
+        acs_data = data_source.get_data(states=state, download=True)
+        self.features = ACSTravelTime.features
+        self.target = ACSTravelTime.target
+        self.categorical_columns = ['MAR','SEX','DIS','ESP','MIG','RELP','RAC1P','PUMA','ST','CIT','OCCP','POWPUMA','POVPIP']
+        self.numerical_columns = ['AGEP', 'SCHL']
+
+        if with_nulls==True:
+            X_data = acs_data[self.features]
+        else:
+            X_data = acs_data[self.features].apply(lambda x: np.nan_to_num(x, -1))
+
+        self.X_data = X_data[self.categorical_columns].astype('str')
+        for col in self.numerical_columns:
+            self.X_data[col] = X_data[col]
+            
+        self.y_data = acs_data[self.target].apply(lambda x: int(x > 20))
+
+        self.columns_with_nulls = self.X_data.columns[self.X_data.isna().any().to_list()].to_list()
+
+    def update_X_data(self, X_data):
+        '''
+        To save simulated nulls
+        '''
+        self.X_data = X_data
+        self.columns_with_nulls = self.X_data.columns[self.X_data.isna().any().to_list()].to_list()
+    
+
+class ACSIncomeDataset():
+    def __init__(self, state, year, with_nulls=False):
+        data_source = ACSDataSource(
+            survey_year=year,
+            horizon='1-Year',
+            survey='person'
+        )
+        acs_data = data_source.get_data(states=state, download=True)
+        self.features = ACSIncome.features
+        self.target = ACSIncome.target
+        self.categorical_columns = ['COW','MAR','OCCP','POBP','RELP','WKHP','SEX','RAC1P']
+        self.numerical_columns = ['AGEP', 'SCHL']
+
+        if with_nulls==True:
+            X_data = acs_data[self.features]
+        else:
+            X_data = acs_data[self.features].apply(lambda x: np.nan_to_num(x, -1))
+
+        self.X_data = X_data[self.categorical_columns].astype('str')
+        for col in self.numerical_columns:
+            self.X_data[col] = X_data[col]
+            
+        self.y_data = acs_data[self.target].apply(lambda x: int(x > 50000))
+
+        self.columns_with_nulls = self.X_data.columns[self.X_data.isna().any().to_list()].to_list()
+
+    def update_X_data(self, X_data):
+        '''
+        To save simulated nulls
+        '''
+        self.X_data = X_data
+        self.columns_with_nulls = self.X_data.columns[self.X_data.isna().any().to_list()].to_list()
+
+class ACSEmploymentDataset():
+    def __init__(self, state, year, with_nulls=False, optimize=True):
+        '''
+        Loading task data: instead of using the task wrapper, we subsample the acs_data dataframe on the task features
+        We do this to retain the nulls as task wrappers handle nulls by imputing as a special category
+        Alternatively, we could have altered the configuration from here:
+        https://github.com/zykls/folktables/blob/main/folktables/acs.py
+        '''
+        data_source = ACSDataSource(
+            survey_year=year,
+            horizon='1-Year',
+            survey='person'
+        )
+        acs_data = data_source.get_data(states=state, download=True)
+        self.features = ACSEmployment.features
+        self.target = ACSEmployment.target
+        self.categorical_columns = ['MAR', 'MIL', 'ESP', 'MIG', 'DREM', 'NATIVITY', 'DIS', 'DEAR', 'DEYE', 'SEX', 'RAC1P', 'RELP', 'CIT', 'ANC','SCHL']
+        self.numerical_columns = ['AGEP']
+
+        if with_nulls==True:
+            X_data = acs_data[self.features]
+        else:
+            X_data = acs_data[self.features].apply(lambda x: np.nan_to_num(x, -1))
+
+        if optimize==True:
+            X_data = optimize_data_loading(X_data, self.categorical_columns)
+
+        self.X_data = X_data[self.categorical_columns].astype('str')
+        for col in self.numerical_columns:
+            self.X_data[col] = X_data[col]
+        self.y_data = acs_data[self.target].apply(lambda x: int(x == 1))
+
+        self.columns_with_nulls = self.X_data.columns[self.X_data.isna().any().to_list()].to_list()
+
+    def update_X_data(self, X_data):
+        '''
+        To save simulated nulls
+        '''
+        self.X_data = X_data
+        self.columns_with_nulls = self.X_data.columns[self.X_data.isna().any().to_list()].to_list()
+
+
+class ACSDataset_from_demodq():
+    ''' Following https://github.com/schelterlabs/demographic-data-quality '''
+    def __init__(self, state, year, with_nulls=False, optimize=True):
+        '''
+        Loading task data: instead of using the task wrapper, we subsample the acs_data dataframe on the task features
+        We do this to retain the nulls as task wrappers handle nulls by imputing as a special category
+        Alternatively, we could have altered the configuration from here:
+        https://github.com/zykls/folktables/blob/main/folktables/acs.py
+        '''
+        data_source = ACSDataSource(
+            survey_year=year,
+            horizon='1-Year',
+            survey='person'
+        )
+        acs_data = data_source.get_data(states=state, download=True)
+        self.features =  ['AGEP', 'COW', 'SCHL', 'MAR', 'OCCP', 'POBP', 'RELP', 'WKHP', 'SEX', 'RAC1P']
+        self.target = ['PINCP']
+        self.categorical_columns = ['COW', 'SCHL', 'MAR', 'OCCP', 'POBP', 'RELP', 'SEX', 'RAC1P']
+        self.numerical_columns = ['AGEP', 'WKHP']
+
+        if with_nulls==True:
+            X_data = acs_data[self.features]
+        else:
+            X_data = acs_data[self.features].apply(lambda x: np.nan_to_num(x, -1))
+
+        if optimize==True:
+            X_data = optimize_data_loading(X_data, self.categorical_columns)
+
+        self.X_data = X_data
+        self.y_data = acs_data[self.target].apply(lambda x: x >= 50000).astype(int)
+
+        self.columns_with_nulls = self.X_data.columns[self.X_data.isna().any().to_list()].to_list()
+
+    def update_X_data(self, X_data):
+        '''
+        To save simulated nulls
+        '''
+        self.X_data = X_data
+        self.columns_with_nulls = self.X_data.columns[self.X_data.isna().any().to_list()].to_list()
+
+
+def optimize_data_loading(data, categorical):
+    '''
+    Optimizing the dataset size by downcasting categorical columns
+    '''
+    for column in categorical:
+        data[column] = pd.to_numeric(data[column], downcast='integer')
+    return data
+
