ffdf7473a12cbd058bbe4b9a9bfd0eed43d6496f
Denys Herasymuk <herasymuk@ucu.edu.ua>
2023-01-15 19:01:16 +0200

    Added bias-variance analysis for COMPAS

--- configs/config.py
+++ configs/config.py
@@ -17,15 +17,15 @@ DATASET_CONFIG = {
 
 
 MODELS_CONFIG = [
-    {
-        'model_name': 'LogisticRegression',
-        'model': LogisticRegression(random_state=SEED),
-        'params': {
-            'penalty': ['none', 'l2'],
-            'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],
-            'max_iter': range(50, 251, 50),
-        }
-    },
+    # {
+    #     'model_name': 'LogisticRegression',
+    #     'model': LogisticRegression(random_state=SEED),
+    #     'params': {
+    #         'penalty': ['none', 'l2'],
+    #         'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],
+    #         'max_iter': range(50, 251, 50),
+    #     }
+    # },
     {
         'model_name': 'DecisionTreeClassifier',
         'model': DecisionTreeClassifier(random_state=SEED),
@@ -35,43 +35,43 @@ MODELS_CONFIG = [
             "criterion": ["gini", "entropy"]
         }
     },
-    {
-        'model_name': 'RandomForestClassifier',
-        'model': RandomForestClassifier(random_state=SEED),
-        'params': {
-            "max_depth": [3, 4, 6, 10],
-            "min_samples_leaf": [1, 2, 4],
-            "n_estimators": [10, 20, 50, 100],
-            "max_features": [0.6, 'auto', 'sqrt']
-        }
-    },
-    {
-        'model_name': 'XGBClassifier',
-        'model': XGBClassifier(random_state=SEED, verbosity = 0),
-        'params': {
-            'learning_rate': [0.1],
-            'n_estimators': [100, 200, 300],
-            'max_depth': range(5, 16, 5),
-            'objective':  ['binary:logistic'],
-        }
-    },
-    {
-        'model_name': 'KNeighborsClassifier',
-        'model': KNeighborsClassifier(),
-        'params': {
-            'n_neighbors' : [5, 7, 9, 11, 13, 15],
-            'weights' : ['uniform', 'distance'],
-            'metric' : ['minkowski', 'euclidean', 'manhattan']
-        }
-    },
     # {
-    #     'model_name': 'MLPClassifier_1L_100',
-    #     'model': MLPClassifier(hidden_layer_sizes=(100,)),
-    #     'params': {}
+    #     'model_name': 'RandomForestClassifier',
+    #     'model': RandomForestClassifier(random_state=SEED),
+    #     'params': {
+    #         "max_depth": [3, 4, 6, 10],
+    #         "min_samples_leaf": [1, 2, 4],
+    #         "n_estimators": [10, 20, 50, 100],
+    #         "max_features": [0.6, 'auto', 'sqrt']
+    #     }
     # },
     # {
-    #     'model_name': 'MLPClassifier_3L_100_50_100',
-    #     'model': MLPClassifier(hidden_layer_sizes=(100, 50, 100)),
-    #     'params': {}
+    #     'model_name': 'XGBClassifier',
+    #     'model': XGBClassifier(random_state=SEED, verbosity = 0),
+    #     'params': {
+    #         'learning_rate': [0.1],
+    #         'n_estimators': [100, 200, 300],
+    #         'max_depth': range(5, 16, 5),
+    #         'objective':  ['binary:logistic'],
+    #     }
     # },
+    # {
+    #     'model_name': 'KNeighborsClassifier',
+    #     'model': KNeighborsClassifier(),
+    #     'params': {
+    #         'n_neighbors' : [5, 7, 9, 11, 13, 15],
+    #         'weights' : ['uniform', 'distance'],
+    #         'metric' : ['minkowski', 'euclidean', 'manhattan']
+    #     }
+    # },
+    # # {
+    # #     'model_name': 'MLPClassifier_1L_100',
+    # #     'model': MLPClassifier(hidden_layer_sizes=(100,)),
+    # #     'params': {}
+    # # },
+    # # {
+    # #     'model_name': 'MLPClassifier_3L_100_50_100',
+    # #     'model': MLPClassifier(hidden_layer_sizes=(100, 50, 100)),
+    # #     'params': {}
+    # # },
 ]
