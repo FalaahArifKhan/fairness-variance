a178e19db40fd97da19487f1b96ccef3672da9de
Denys Herasymuk <herasymuk@ucu.edu.ua>
2023-01-21 23:56:26 +0200

    Returned to the previous metrics computation pipeline

--- configs/config.py
+++ configs/config.py
@@ -18,16 +18,6 @@ DATASET_CONFIG = {
 
 MODELS_CONFIG = [
     {
-        'model_name': 'LogisticRegression',
-        'model': LogisticRegression(random_state=SEED),
-        'params': {
-            'penalty': ['l1', 'l2'],
-            'C' : [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100],
-            'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],
-            'max_iter': range(50, 251, 50),
-        }
-    },
-    {
         'model_name': 'DecisionTreeClassifier',
         'model': DecisionTreeClassifier(random_state=SEED),
         'params': {
@@ -37,43 +27,53 @@ MODELS_CONFIG = [
             "criterion": ["gini", "entropy"]
         }
     },
-    {
-        'model_name': 'RandomForestClassifier',
-        'model': RandomForestClassifier(random_state=SEED),
-        'params': {
-            "max_depth": [3, 4, 6, 10],
-            "min_samples_leaf": [1, 2, 4],
-            "n_estimators": [50, 100, 500, 700],
-            "max_features": [0.6, 'auto', 'sqrt']
-        }
-    },
-    {
-        'model_name': 'XGBClassifier',
-        'model': XGBClassifier(random_state=SEED, verbosity = 0),
-        'params': {
-            'learning_rate': [0.1],
-            'n_estimators': [100, 200, 300, 500],
-            'max_depth': [3,5,7,10],
-            'lambda':  [1,10,100]
-        }
-    },
-    {
-        'model_name': 'KNeighborsClassifier',
-        'model': KNeighborsClassifier(),
-        'params': {
-            'n_neighbors' : [5, 7, 9, 11, 13, 15, 25],
-            'weights' : ['uniform', 'distance'],
-            'metric' : ['minkowski', 'euclidean', 'manhattan']
-        }
-    },
-    {
-        'model_name': 'MLPClassifier',
-        'model': MLPClassifier(random_state=SEED),
-        'params': {
-            'hidden_layer_sizes':[(100,), (100,100,), (100,50,100,)],
-            'activation': ['logistic', 'tanh', 'relu'],
-            'solver': ['lbfgs', 'sgd', 'adam'],
-            'learning_rate': ['constant', 'invscaling', 'adaptive']
-        }
-    }
+    # {
+    #     'model_name': 'LogisticRegression',
+    #     'model': LogisticRegression(random_state=SEED),
+    #     'params': {
+    #         'penalty': ['l1', 'l2'],
+    #         'C' : [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100],
+    #         'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],
+    #         'max_iter': range(50, 251, 50),
+    #     }
+    # },
+    # {
+    #     'model_name': 'RandomForestClassifier',
+    #     'model': RandomForestClassifier(random_state=SEED),
+    #     'params': {
+    #         "max_depth": [3, 4, 6, 10],
+    #         "min_samples_leaf": [1, 2, 4],
+    #         "n_estimators": [50, 100, 500, 700],
+    #         "max_features": [0.6, 'auto', 'sqrt']
+    #     }
+    # },
+    # {
+    #     'model_name': 'XGBClassifier',
+    #     'model': XGBClassifier(random_state=SEED, verbosity=0),
+    #     'params': {
+    #         'learning_rate': [0.1],
+    #         'n_estimators': [100, 200, 300, 500],
+    #         'max_depth': [3,5,7,10],
+    #         'lambda':  [1,10,100]
+    #     }
+    # },
+    # {
+    #     'model_name': 'KNeighborsClassifier',
+    #     'model': KNeighborsClassifier(),
+    #     'params': {
+    #         'n_neighbors' : [5, 7, 9, 11, 13, 15, 25],
+    #         'weights' : ['uniform', 'distance'],
+    #         'metric' : ['minkowski', 'euclidean', 'manhattan']
+    #     }
+    # },
+    # {
+    #     'model_name': 'MLPClassifier',
+    #     'model': MLPClassifier(random_state=SEED),
+    #     'params': {
+    #         'hidden_layer_sizes':[(100,), (100,100,), (100,50,100,)],
+    #         'activation': ['logistic', 'tanh', 'relu'],
+    #         'solver': ['lbfgs', 'sgd', 'adam'],
+    #         'learning_rate': ['constant', 'invscaling', 'adaptive']
+    #     }
+    # }
 ]
