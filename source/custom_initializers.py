48a1a7a65986b138a2fa99bfa9e8787534f3bf1d
Denys Herasymuk <herasymuk@ucu.edu.ua>
2023-04-07 01:19:39 +0300

    Added a template for RandomNullsGenerator usage

--- source/custom_initializers.py
+++ source/custom_initializers.py
@@ -5,6 +5,7 @@ def create_experiment_data_loader(data_loader, generator=None):
     exp_iter_data_loader = copy.deepcopy(data_loader)  # Add deepcopy to avoid data leakage
     transformed_df = generator.fit_transform(exp_iter_data_loader.full_df, exp_iter_data_loader.target)
 
+    # Reset data loader attributes
     exp_iter_data_loader.full_df = transformed_df
     exp_iter_data_loader.X_data = transformed_df[exp_iter_data_loader.features]
     exp_iter_data_loader.y_data = transformed_df[exp_iter_data_loader.target]
