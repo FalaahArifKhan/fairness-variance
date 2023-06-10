0564dd7527db19fad2bad09a41863e05948f2644
Denys Herasymuk <herasymuk@ucu.edu.ua>
2023-06-11 02:26:22 +0300

    Created a structure for the repo

--- configs/models_config_for_tuning.py
+++ configs/models_config_for_tuning.py
@@ -7,8 +7,46 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 
 
-MODELS_TUNING_SEED = 42
-MODELS_TUNING_TEST_SET_FRACTION = 0.2
+def get_compas_models_params_for_tuning(models_tuning_seed):
+    return {
+        'DecisionTreeClassifier': {
+            'model': DecisionTreeClassifier(random_state=models_tuning_seed),
+            'params': {
+                "max_depth": [20, 30],
+                "min_samples_split" : [0.1],
+                "max_features": ['sqrt'],
+                "criterion": ["gini", "entropy"]
+            }
+        },
+        'LogisticRegression': {
+            'model': LogisticRegression(random_state=models_tuning_seed),
+            'params': {
+                'penalty': ['l2'],
+                'C' : [0.0001, 0.1, 1, 100],
+                'solver': ['newton-cg', 'lbfgs'],
+                'max_iter': [250],
+            }
+        },
+        'RandomForestClassifier': {
+            'model': RandomForestClassifier(random_state=models_tuning_seed),
+            'params': {
+                "max_depth": [6, 10],
+                "min_samples_leaf": [1],
+                "n_estimators": [50, 100],
+                "max_features": [0.6]
+            }
+        },
+        'XGBClassifier': {
+            'model': XGBClassifier(random_state=models_tuning_seed, verbosity=0),
+            'params': {
+                'learning_rate': [0.1],
+                'n_estimators': [200],
+                'max_depth': [5, 7],
+                'lambda':  [10, 100]
+            }
+        }
+    }
+
 
 '''
 MODELS_CONFIG = [
@@ -75,15 +113,15 @@ MODELS_CONFIG = [
 ]
 '''
 
-MODELS_CONFIG = [
-    {
-        'model_name': 'MLPClassifier',
-        'model': MLPClassifier(random_state=MODELS_TUNING_SEED, max_iter=1000),
-        'params': {
-            'hidden_layer_sizes':[(100,), (100,100,), (100,50,100,)],
-            'activation': ['logistic', 'tanh', 'relu'],
-            'solver': ['lbfgs', 'sgd', 'adam'],
-            'learning_rate': ['constant', 'invscaling', 'adaptive']
-        }
-    }
-]
+# MODELS_CONFIG = [
+#     {
+#         'model_name': 'MLPClassifier',
+#         'model': MLPClassifier(random_state=MODELS_TUNING_SEED, max_iter=1000),
+#         'params': {
+#             'hidden_layer_sizes':[(100,), (100,100,), (100,50,100,)],
+#             'activation': ['logistic', 'tanh', 'relu'],
+#             'solver': ['lbfgs', 'sgd', 'adam'],
+#             'learning_rate': ['constant', 'invscaling', 'adaptive']
+#         }
+#     }
+# ]
