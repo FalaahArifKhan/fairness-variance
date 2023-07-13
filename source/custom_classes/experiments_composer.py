726c3727cd6d0f1b33c4aa422f049944c73a780b
Denys Herasymuk <herasymuk@ucu.edu.ua>
2023-07-14 01:17:23 +0300

    Added preprocessing and box plots

--- source/custom_classes/experiments_composer.py
+++ source/custom_classes/experiments_composer.py
@@ -50,6 +50,39 @@ class ExperimentsComposer:
 
         return structured_exp_results_dct
 
+    def create_exp_subgroup_metrics_dct_for_mult_test_sets(self):
+        structured_exp_results_dct = dict()
+        for model_name in self.models_metrics_dct.keys():
+            structured_exp_results_dct[model_name] = dict()
+            distinct_exp_iters_lst = self.models_metrics_dct[model_name]['Experiment_Iteration'].unique()
+
+            for exp_iter in distinct_exp_iters_lst:
+                structured_exp_results_dct[model_name][exp_iter] = dict()
+                distinct_intervention_params = \
+                    self.models_metrics_dct[model_name][
+                        (self.models_metrics_dct[model_name]['Experiment_Iteration'] == exp_iter)
+                    ]['Intervention_Param'].unique()
+
+                for intervention_param in distinct_intervention_params:
+                    structured_exp_results_dct[model_name][exp_iter][intervention_param] = dict()
+                    distinct_test_set_indexes = \
+                        self.models_metrics_dct[model_name][
+                            (self.models_metrics_dct[model_name]['Experiment_Iteration'] == exp_iter) &
+                            (self.models_metrics_dct[model_name]['Intervention_Param'] == intervention_param)
+                        ]['Test_Set_Index'].unique()
+
+                    for test_set_index in distinct_test_set_indexes:
+                        model_metrics_per_run_per_percentage = \
+                            self.models_metrics_dct[model_name][
+                                (self.models_metrics_dct[model_name]['Experiment_Iteration'] == exp_iter) &
+                                (self.models_metrics_dct[model_name]['Intervention_Param'] == intervention_param) &
+                                (self.models_metrics_dct[model_name]['Test_Set_Index'] == test_set_index)
+                            ]
+                        structured_exp_results_dct[model_name][exp_iter][intervention_param][test_set_index] = \
+                            model_metrics_per_run_per_percentage
+
+        return structured_exp_results_dct
+
     def compose_group_metrics(self, structured_exp_results_dct):
         """
         Create a group metrics dictionary based on the input structured_exp_results_dct.
@@ -89,5 +122,27 @@ class ExperimentsComposer:
                     model_group_metrics_df['Intervention_Param'] = percentage
                     structured_exp_avg_group_metrics_dct.setdefault(model_name, {})\
                         .setdefault(exp_iter, {})[percentage] = model_group_metrics_df
+
+        return structured_exp_avg_group_metrics_dct
+
+    def compose_group_metrics_for_mult_test_sets(self, structured_exp_results_dct):
+        structured_exp_avg_group_metrics_dct = dict()
+        for model_name in structured_exp_results_dct.keys():
+            for exp_iter in structured_exp_results_dct[model_name].keys():
+                for intervention_param in structured_exp_results_dct[model_name][exp_iter].keys():
+                    for test_set_index in structured_exp_results_dct[model_name][exp_iter][intervention_param].keys():
+                        model_subgroup_metrics_df = structured_exp_results_dct[model_name][exp_iter][intervention_param][test_set_index]
+                        model_subgroup_metrics_df = model_subgroup_metrics_df.drop(['Bootstrap_Model_Seed', 'Record_Create_Date_Time'], axis=1)
+                        metrics_composer = MetricsComposer(
+                            {model_name: model_subgroup_metrics_df},
+                            self.sensitive_attributes_dct
+                        )
+                        model_group_metrics_df = metrics_composer.compose_metrics()
+                        model_group_metrics_df['Experiment_Iteration'] = exp_iter
+                        model_group_metrics_df['Intervention_Param'] = intervention_param
+                        model_group_metrics_df['Test_Set_Index'] = test_set_index
+                        structured_exp_avg_group_metrics_dct.setdefault(model_name, {}) \
+                            .setdefault(exp_iter, {}) \
+                            .setdefault(intervention_param, {})[test_set_index] = model_group_metrics_df
 
         return structured_exp_avg_group_metrics_dct
---
+++ source/visualizations.py
@@ -0,0 +1,145 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def preprocess_metrics(exp_subgroup_metrics_dct, exp_group_metrics_dct):
+    # Create melted_exp_subgroup_metrics_dct
+    melted_exp_subgroup_metrics_dct = dict()
+    for model_name in exp_subgroup_metrics_dct.keys():
+        for exp_iter in exp_subgroup_metrics_dct[model_name].keys():
+            for intervention_param in exp_subgroup_metrics_dct[model_name][exp_iter].keys():
+                for test_set_index in exp_subgroup_metrics_dct[model_name][exp_iter][intervention_param].keys():
+                    model_subgroup_metrics_df = exp_subgroup_metrics_dct[model_name][exp_iter][intervention_param][test_set_index]
+                    subgroup_names = [col for col in model_subgroup_metrics_df.columns if '_priv' in col or '_dis' in col] + ['overall']
+                    melted_model_subgroup_metrics_df = model_subgroup_metrics_df.melt(
+                        id_vars=[col for col in model_subgroup_metrics_df.columns if col not in subgroup_names],
+                        value_vars=subgroup_names,
+                        var_name="Subgroup",
+                        value_name="Metric_Value"
+                    )
+                    melted_exp_subgroup_metrics_dct.setdefault(model_name, {}) \
+                        .setdefault(exp_iter, {}).setdefault(intervention_param, {})[test_set_index] = melted_model_subgroup_metrics_df
+
+    # Create a dict where a key is a model name and a value is a dataframe with
+    # all subgroup metrics, intervention_params, test_set_index, and exp iterations per each model
+    melted_all_subgroup_metrics_per_model_dct = dict()
+    for model_name in melted_exp_subgroup_metrics_dct.keys():
+        all_subgroup_metrics_per_model_df = pd.DataFrame()
+        for exp_iter in melted_exp_subgroup_metrics_dct[model_name].keys():
+            for intervention_param in melted_exp_subgroup_metrics_dct[model_name][exp_iter].keys():
+                for test_set_index in melted_exp_subgroup_metrics_dct[model_name][exp_iter][intervention_param].keys():
+                    all_subgroup_metrics_per_model_df = pd.concat([
+                        all_subgroup_metrics_per_model_df,
+                        melted_exp_subgroup_metrics_dct[model_name][exp_iter][intervention_param][test_set_index],
+                    ])
+        melted_all_subgroup_metrics_per_model_dct[model_name] = all_subgroup_metrics_per_model_df
+
+    # Create melted_exp_group_metrics_dct
+    melted_exp_group_metrics_dct = dict()
+    for model_name in exp_group_metrics_dct.keys():
+        for exp_iter in exp_group_metrics_dct[model_name].keys():
+            for intervention_param in exp_group_metrics_dct[model_name][exp_iter].keys():
+                for test_set_index in exp_group_metrics_dct[model_name][exp_iter][intervention_param].keys():
+                    model_group_metrics_df = exp_group_metrics_dct[model_name][exp_iter][intervention_param][test_set_index]
+                    # All other columns in model_group_metrics_df
+                    # except 'Metric', 'Model_Name', 'Intervention_Param', 'Experiment_Iteration' are group names
+                    group_names = [col for col in model_group_metrics_df.columns
+                                   if col not in ('Metric', 'Model_Name', 'Intervention_Param', 'Test_Set_Index', 'Experiment_Iteration')]
+                    melted_model_group_metrics_df = model_group_metrics_df.melt(
+                        id_vars=[col for col in model_group_metrics_df.columns if col not in group_names],
+                        value_vars=group_names,
+                        var_name="Group",
+                        value_name="Metric_Value"
+                    )
+                    melted_exp_group_metrics_dct.setdefault(model_name, {}) \
+                        .setdefault(exp_iter, {}).setdefault(intervention_param, {})[test_set_index] = melted_model_group_metrics_df
+
+    # Create a dict where a key is a model name and a value is a dataframe with
+    # all group metrics, intervention_params, test_set_index, and exp iterations per each model
+    melted_all_group_metrics_per_model_dct = dict()
+    for model_name in melted_exp_group_metrics_dct.keys():
+        all_group_metrics_per_model_df = pd.DataFrame()
+        for exp_iter in melted_exp_group_metrics_dct[model_name].keys():
+            for intervention_param in melted_exp_group_metrics_dct[model_name][exp_iter].keys():
+                for test_set_index in melted_exp_group_metrics_dct[model_name][exp_iter][intervention_param].keys():
+                    all_group_metrics_per_model_df = pd.concat([
+                        all_group_metrics_per_model_df,
+                        melted_exp_group_metrics_dct[model_name][exp_iter][intervention_param][test_set_index],
+                    ])
+        melted_all_group_metrics_per_model_dct[model_name] = all_group_metrics_per_model_df
+
+
+    return melted_all_subgroup_metrics_per_model_dct, melted_all_group_metrics_per_model_dct
+
+
+def create_subgroup_base_and_fair_models_box_plot(all_subgroup_metrics_per_model_dct: dict, subgroup_metric_name: str,
+                                                  legend_location: str = 'upper left'):
+    sns.set_style("darkgrid")
+
+    # Create one metrics df with all model_dfs
+    all_models_metrics_df = pd.DataFrame()
+    for model_name in all_subgroup_metrics_per_model_dct.keys():
+        model_metrics_df = all_subgroup_metrics_per_model_dct[model_name]
+        all_models_metrics_df = pd.concat([all_models_metrics_df, model_metrics_df])
+
+    all_models_metrics_df = all_models_metrics_df.reset_index(drop=True)
+
+    to_plot = all_models_metrics_df[
+        (all_models_metrics_df['Metric'] == subgroup_metric_name) &
+        (all_models_metrics_df['Subgroup'] == 'overall') &
+        (all_models_metrics_df['Test_Set_Index'] == 0)
+        ]
+
+    plt.figure(figsize=(12, 6))
+    ax = sns.boxplot(x=to_plot['Model_Name'],
+                     y=to_plot['Metric_Value'],
+                     hue=to_plot['Intervention_Param'])
+
+    plt.legend(loc=legend_location,
+               ncol=2,
+               fancybox=True,
+               shadow=True,
+               fontsize=13)
+    plt.xlabel("Model name", fontsize=16)
+    plt.ylabel("Metric value", fontsize=16)
+    ax.tick_params(labelsize=14)
+    fig = ax.get_figure()
+    fig.tight_layout()
+
+
+def create_group_base_and_fair_models_box_plot(all_group_metrics_per_model_dct: dict, group_metric_name: str, group: str,
+                                               legend_location: str = 'upper left'):
+    sns.set_style("darkgrid")
+
+    # Create one metrics df with all model_dfs
+    all_models_metrics_df = pd.DataFrame()
+    for model_name in all_group_metrics_per_model_dct.keys():
+        model_metrics_df = all_group_metrics_per_model_dct[model_name]
+        all_models_metrics_df = pd.concat([all_models_metrics_df, model_metrics_df])
+
+    all_models_metrics_df = all_models_metrics_df.reset_index(drop=True)
+
+    to_plot = all_models_metrics_df[
+        (all_models_metrics_df['Metric'] == group_metric_name) &
+        (all_models_metrics_df['Group'] == group) &
+        (all_models_metrics_df['Test_Set_Index'] == 0)
+    ]
+
+    plt.figure(figsize=(12, 6))
+    ax = sns.boxplot(x=to_plot['Model_Name'],
+                     y=to_plot['Metric_Value'],
+                     hue=to_plot['Intervention_Param'])
+
+    plt.legend(loc=legend_location,
+               title='Alpha',
+               title_fontsize=14,
+               ncol=2,
+               fancybox=True,
+               shadow=True,
+               fontsize=13)
+    plt.xlabel("Model name", fontsize=16)
+    plt.ylabel("Metric value", fontsize=16)
+    ax.tick_params(labelsize=14)
+    fig = ax.get_figure()
+    fig.tight_layout()
