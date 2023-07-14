0aec2ecab2c97b9ea7a3cc8d3b96c89acd6584b8
Denys Herasymuk <herasymuk@ucu.edu.ua>
2023-07-14 13:08:35 +0300

    Added scatter plots

--- source/visualizations.py
+++ source/visualizations.py
@@ -1,6 +1,8 @@
+import altair as alt
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from IPython.display import display
 
 
 def preprocess_metrics(exp_subgroup_metrics_dct, exp_group_metrics_dct):
@@ -109,3 +111,61 @@ def create_group_base_and_fair_models_box_plot(all_group_metrics_per_model_dct: 
     ax.tick_params(labelsize=14)
     fig = ax.get_figure()
     fig.tight_layout()
+
+
+def create_scatter_plot(all_group_metrics_per_model_dct: dict, group: str,
+                        fairness_metric_name: str, stability_metric_name: str,
+                        alpha: float, test_set_index: int = 0):
+    # Create one metrics df with all model_dfs
+    all_models_metrics_df = pd.DataFrame()
+    for model_name in all_group_metrics_per_model_dct.keys():
+        model_metrics_df = all_group_metrics_per_model_dct[model_name]
+        all_models_metrics_df = pd.concat([all_models_metrics_df, model_metrics_df])
+
+    all_models_metrics_df = all_models_metrics_df.reset_index(drop=True)
+    all_models_metrics_df = all_models_metrics_df[
+        (all_models_metrics_df['Group'] == group) &
+        (all_models_metrics_df['Intervention_Param'] == alpha) &
+        (all_models_metrics_df['Test_Set_Index'] == test_set_index)
+    ]
+
+    models_fairness_metric_df = all_models_metrics_df[
+        (all_models_metrics_df['Metric'] == fairness_metric_name)
+    ].reset_index(drop=True)
+    models_stability_metric_df = all_models_metrics_df[
+        (all_models_metrics_df['Metric'] == stability_metric_name)
+    ].reset_index(drop=True)
+
+    to_plot = models_fairness_metric_df
+    to_plot = to_plot.drop(columns=['Metric', 'Metric_Value'])
+    to_plot[fairness_metric_name] = models_fairness_metric_df['Metric_Value']
+    to_plot[stability_metric_name] = models_stability_metric_df['Metric_Value']
+
+    chart = alt.Chart(to_plot).mark_point(size=60).encode(
+        x=stability_metric_name,
+        y=fairness_metric_name,
+        color='Model_Name',
+        shape='Experiment_Iteration',
+        size=alt.value(80),
+    )
+
+    final_chart = (
+        chart.configure_axis(
+            labelFontSize=15 + 2,
+            titleFontSize=15 + 4,
+            labelFontWeight='normal',
+            titleFontWeight='normal',
+        ).configure_title(
+            fontSize=15 + 2
+        ).configure_legend(
+            titleFontSize=17 + 2,
+            labelFontSize=15 + 2,
+            symbolStrokeWidth=4,
+            labelLimit=200,
+            titleLimit=220,
+        ).properties(
+            width=500, height=300
+        )
+    )
+
+    return final_chart
