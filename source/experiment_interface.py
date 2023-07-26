33b4a1ca1de914104af80dfde11dbd64f6f819a2
Denys Herasymuk <herasymuk@ucu.edu.ua>
2023-07-27 00:12:54 +0300

    Prepared everything for exp2 Income 2018

--- source/experiment_interface.py
+++ source/experiment_interface.py
@@ -13,7 +13,8 @@ from virny.preprocessing.basic_preprocessing import preprocess_dataset
 from source.utils.model_tuning_utils import tune_ML_models
 from source.utils.custom_logger import get_logger
 from source.preprocessing import remove_correlation, remove_correlation_for_mult_test_sets, \
-    remove_disparate_impact, get_simple_preprocessor
+    remove_disparate_impact, get_simple_preprocessor, preprocess_mult_data_loaders_for_disp_imp, \
+    remove_disparate_impact_with_mult_sets
 
 
 def run_exp_iter_with_preprocessing_intervention(data_loader, experiment_seed, test_set_fraction,
@@ -155,6 +156,77 @@ def run_exp_iter_with_disparate_impact(data_loader, experiment_seed, test_set_fr
                                                      custom_tbl_fields_dct=custom_table_fields_dct,
                                                      db_writer_func=db_writer_func,
                                                      verbose=0)
+
+    logger.info("Experiment run was successful!")
+
+
+def run_exp_iter_with_disparate_impact_and_mult_sets(data_loader, extra_data_loaders, experiment_seed, test_set_fraction,
+                                                     db_writer_func, fair_intervention_params_lst, models_params_for_tuning,
+                                                     metrics_computation_config, custom_table_fields_dct,
+                                                     with_tuning: bool = False, save_results_dir_path: str = None,
+                                                     tuned_params_df_paths: list = None, num_folds_for_tuning: int = 3,
+                                                     verbose: bool = False):
+    custom_table_fields_dct['dataset_split_seed'] = experiment_seed
+    custom_table_fields_dct['model_init_seed'] = experiment_seed
+    custom_table_fields_dct['fair_intervention_params_lst'] = str(fair_intervention_params_lst)
+
+    logger = get_logger()
+    logger.info(f"Start an experiment iteration for the following custom params:")
+    pprint(custom_table_fields_dct)
+    print('\n', flush=True)
+
+    # Set seeds for metrics computation
+    metrics_computation_config.runs_seed_lst = [experiment_seed + i for i in range(1, metrics_computation_config.num_runs + 1)]
+
+    init_base_flow_dataset, extra_base_flow_datasets = \
+        preprocess_mult_data_loaders_for_disp_imp(data_loaders=[data_loader] + extra_data_loaders,
+                                                  test_set_fraction=test_set_fraction,
+                                                  experiment_seed=experiment_seed)
+    if verbose:
+        logger.info("The dataset is preprocessed")
+        print("Top indexes of an X_test in a base flow dataset: ", init_base_flow_dataset.X_test.index[:20])
+        print("Top indexes of an y_test in a base flow dataset: ", init_base_flow_dataset.y_test.index[:20])
+
+    for intervention_idx, intervention_param in tqdm(enumerate(fair_intervention_params_lst),
+                                                     total=len(fair_intervention_params_lst),
+                                                     desc="Multiple alphas",
+                                                     colour="#40E0D0"):
+        print('intervention_param: ', intervention_param)
+        custom_table_fields_dct['intervention_param'] = intervention_param
+
+        # Fair preprocessing
+        cur_base_flow_dataset, cur_extra_test_sets =\
+            remove_disparate_impact_with_mult_sets(init_base_flow_dataset, alpha=intervention_param,
+                                                   init_extra_base_flow_datasets=extra_base_flow_datasets)
+
+        # Tune model parameters if needed
+        if with_tuning:
+            # Tune models and create a models config for metrics computation
+            tuned_params_df, models_config = tune_ML_models(models_params_for_tuning, cur_base_flow_dataset,
+                                                            metrics_computation_config.dataset_name,
+                                                            n_folds=num_folds_for_tuning)
+
+            # Create models_config from the saved tuned_params_df for higher reliability
+            date_time_str = datetime.now(timezone.utc).strftime("%Y%m%d__%H%M%S")
+            os.makedirs(save_results_dir_path, exist_ok=True)
+            tuned_df_path = os.path.join(save_results_dir_path,
+                                         f'tuning_results_{metrics_computation_config.dataset_name}_alpha_{intervention_param}_{date_time_str}.csv')
+            tuned_params_df.to_csv(tuned_df_path, sep=",", columns=tuned_params_df.columns, float_format="%.4f", index=False)
+            logger.info("Models are tuned and saved to a file")
+        else:
+            print('Path for tuned params: ', tuned_params_df_paths[intervention_idx])
+            models_config = create_models_config_from_tuned_params_df(models_params_for_tuning, tuned_params_df_paths[intervention_idx])
+            print(f'{list(models_config.keys())[0]}: ', models_config[list(models_config.keys())[0]].get_params())
+            logger.info("Models config is loaded from the input file")
+
+        # Compute metrics for tuned models
+        compute_metrics_multiple_runs_with_multiple_test_sets(dataset=cur_base_flow_dataset,
+                                                              extra_test_sets_lst=cur_extra_test_sets,
+                                                              config=metrics_computation_config,
+                                                              models_config=models_config,
+                                                              custom_tbl_fields_dct=custom_table_fields_dct,
+                                                              db_writer_func=db_writer_func,
+                                                              verbose=0)
 
     logger.info("Experiment run was successful!")
 
--- source/preprocessing.py
+++ source/preprocessing.py
@@ -26,6 +26,29 @@ def create_extra_test_sets(extra_data_loaders: list, column_transformer, test_se
     return extra_test_sets
 
 
+def preprocess_mult_data_loaders_for_disp_imp(data_loaders: list, test_set_fraction: float, experiment_seed: int):
+    base_flow_datasets = []
+    for cur_data_loader in data_loaders:
+        # Add RACE column for DisparateImpactRemover and remove 'SEX', 'RAC1P' to create a blind estimator
+        init_data_loader = copy.deepcopy(cur_data_loader)
+        cur_data_loader.categorical_columns = [col for col in cur_data_loader.categorical_columns if col not in ('SEX', 'RAC1P')]
+        cur_data_loader.X_data['RACE'] = cur_data_loader.X_data['RAC1P'].apply(lambda x: 1 if x == '1' else 0)
+        cur_data_loader.full_df = cur_data_loader.full_df.drop(['SEX', 'RAC1P'], axis=1)
+        cur_data_loader.X_data = cur_data_loader.X_data.drop(['SEX', 'RAC1P'], axis=1)
+
+        # Preprocess the dataset using the defined preprocessor
+        column_transformer = get_simple_preprocessor(cur_data_loader)
+        cur_base_flow_dataset = preprocess_dataset(cur_data_loader, column_transformer, test_set_fraction, experiment_seed)
+        cur_base_flow_dataset.init_features_df = init_data_loader.full_df.drop(init_data_loader.target, axis=1, errors='ignore')
+        cur_base_flow_dataset.X_train_val['RACE'] = cur_data_loader.X_data.loc[cur_base_flow_dataset.X_train_val.index, 'RACE']
+        cur_base_flow_dataset.X_test['RACE'] = cur_data_loader.X_data.loc[cur_base_flow_dataset.X_test.index, 'RACE']
+
+        base_flow_datasets.append(cur_base_flow_dataset)
+
+    init_base_flow_dataset, extra_base_flow_datasets = base_flow_datasets[0], base_flow_datasets[1:]
+    return init_base_flow_dataset, extra_base_flow_datasets
+
+
 def remove_disparate_impact(init_base_flow_dataset, alpha):
     """
     Based on this documentation:
@@ -62,6 +85,84 @@ def remove_disparate_impact(init_base_flow_dataset, alpha):
     base_flow_dataset.y_test = test_repaired_df[base_flow_dataset.target]
 
     return base_flow_dataset
+
+
+def remove_disparate_impact_with_mult_sets(init_base_flow_dataset, alpha, init_extra_base_flow_datasets):
+    """
+    Based on this documentation:
+     https://aif360.readthedocs.io/en/latest/modules/generated/aif360.algorithms.preprocessing.DisparateImpactRemover.html
+
+    """
+    base_flow_dataset = copy.deepcopy(init_base_flow_dataset)
+    extra_base_flow_datasets = copy.deepcopy(init_extra_base_flow_datasets)
+    sensitive_attribute = 'RACE'
+    other_feature_columns = [col for col in base_flow_dataset.X_train_val.columns if col != sensitive_attribute]
+
+    # Rearrange columns for consistency
+    base_flow_dataset.X_train_val = base_flow_dataset.X_train_val[other_feature_columns + [sensitive_attribute]]
+
+    # Align columns in the test sets with the main train set
+    base_flow_datasets = [base_flow_dataset] + extra_base_flow_datasets
+    aligned_base_flow_datasets = []
+    for i in range(len(base_flow_datasets)):
+        cur_base_flow_ds = base_flow_datasets[i]
+        for col in other_feature_columns:
+            if col not in cur_base_flow_ds.X_test.columns:
+                cur_base_flow_ds.X_test[col] = 0.0
+
+        cur_base_flow_ds.X_test = cur_base_flow_ds.X_test[other_feature_columns + [sensitive_attribute]]
+        aligned_base_flow_datasets.append(cur_base_flow_ds)
+
+    main_base_flow_dataset = base_flow_datasets[0]
+    extra_aligned_base_flow_datasets = base_flow_datasets[1:]
+
+    # Apply Disparate Impact Remover on the in-domain dataset
+    train_df = main_base_flow_dataset.X_train_val
+    train_df[main_base_flow_dataset.target] = main_base_flow_dataset.y_train_val
+    test_df = main_base_flow_dataset.X_test
+    test_df[main_base_flow_dataset.target] = main_base_flow_dataset.y_test
+
+    train_binary_dataset = BinaryLabelDataset(df=train_df,
+                                              label_names=[main_base_flow_dataset.target],
+                                              protected_attribute_names=[sensitive_attribute],
+                                              favorable_label=1,
+                                              unfavorable_label=0)
+    test_binary_dataset = BinaryLabelDataset(df=test_df,
+                                             label_names=[main_base_flow_dataset.target],
+                                             protected_attribute_names=[sensitive_attribute],
+                                             favorable_label=1,
+                                             unfavorable_label=0)
+
+    di = DisparateImpactRemover(repair_level=alpha, sensitive_attribute=sensitive_attribute)
+    train_repaired_df, _ = di.fit_transform(train_binary_dataset).convert_to_dataframe()
+    test_repaired_df , _ = di.fit_transform(test_binary_dataset).convert_to_dataframe()
+    train_repaired_df.index = train_repaired_df.index.astype(dtype='int64')
+    test_repaired_df.index = test_repaired_df.index.astype(dtype='int64')
+
+    main_base_flow_dataset.X_train_val = train_repaired_df.drop([main_base_flow_dataset.target, sensitive_attribute], axis=1)
+    main_base_flow_dataset.y_train_val = train_repaired_df[main_base_flow_dataset.target]
+    main_base_flow_dataset.X_test = test_repaired_df.drop([main_base_flow_dataset.target, sensitive_attribute], axis=1)
+    main_base_flow_dataset.y_test = test_repaired_df[main_base_flow_dataset.target]
+
+    # Apply Disparate Impact Remover on the out-of-domain datasets
+    extra_test_sets = []
+    for aligned_ds in extra_aligned_base_flow_datasets:
+        test_df = aligned_ds.X_test
+        test_df[aligned_ds.target] = aligned_ds.y_test
+
+        test_binary_dataset = BinaryLabelDataset(df=test_df,
+                                                 label_names=[aligned_ds.target],
+                                                 protected_attribute_names=[sensitive_attribute],
+                                                 favorable_label=1,
+                                                 unfavorable_label=0)
+        test_repaired_df , _ = di.fit_transform(test_binary_dataset).convert_to_dataframe()
+        test_repaired_df.index = test_repaired_df.index.astype(dtype='int64')
+
+        extra_X_test = test_repaired_df.drop([aligned_ds.target, sensitive_attribute], axis=1)
+        extra_y_test = test_repaired_df[aligned_ds.target]
+        extra_test_sets.append((extra_X_test, extra_y_test))
+
+    return main_base_flow_dataset, extra_test_sets
 
 
 def remove_correlation(init_base_flow_dataset, alpha):
