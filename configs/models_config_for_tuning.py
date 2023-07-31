820f4c45c87fd5d48aece7376fb9f71b593fa3de
Denys Herasymuk <herasymuk@ucu.edu.ua>
2023-07-31 20:04:57 +0300

    Prepared files for exp 1 for ACSPubCov

--- configs/models_config_for_tuning.py
+++ configs/models_config_for_tuning.py
@@ -48,6 +48,21 @@ def get_folktables_employment_models_params_for_tuning(models_tuning_seed):
     }
 
 
+def get_model_params_for_mult_repair_levels(models_tuning_seed):
+    return {
+        'RandomForestClassifier': {
+            'model': RandomForestClassifier(random_state=models_tuning_seed),
+            'params': {
+                'n_estimators': [100, 200, 500],
+                'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
+                'min_samples_split': [2, 5, 10],
+                'min_samples_leaf': [1, 2, 4],
+                'bootstrap': [True, False]
+            }
+        },
+    }
+
+
 def get_folktables_employment_models_params_for_tuning2(models_tuning_seed):
     return {
         'MLPClassifier': {
