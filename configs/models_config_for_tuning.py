b2662b408d77a6db889e4ad0868248c3549994f6
denysgerasymuk799 <herasymuk@ucu.edu.ua>
2023-08-06 19:08:52 -0400

    Added results for exp 1 Law School

--- configs/models_config_for_tuning.py
+++ configs/models_config_for_tuning.py
@@ -10,22 +10,22 @@ from xgboost import XGBClassifier
 
 def get_folktables_employment_models_params_for_tuning(models_tuning_seed):
     return {
-        'LGBMClassifier': {
-            'model': LGBMClassifier(random_state=models_tuning_seed),
-            'params': {
-                'max_depth' : [i for i in range(3,12)],
-                'num_leaves' : [int(x) for x in np.linspace(start = 20, stop = 3000, num = 10)],
-                'min_data_in_leaf' : [int(x) for x in np.linspace(start = 100, stop = 1000, num = 10)]
-            }
-        },
-        'LogisticRegression': {
-            'model': LogisticRegression(random_state=models_tuning_seed, max_iter=1000),
-            'params': {
-                'penalty': ['l1', 'l2'],
-                'C' : [0.001, 0.01, 0.1, 1],
-                'solver': ['newton-cg', 'lbfgs', 'sag', 'saga'],
-            }
-        },
+#         'LGBMClassifier': {
+#             'model': LGBMClassifier(random_state=models_tuning_seed),
+#             'params': {
+#                 'max_depth' : [i for i in range(3,12)],
+#                 'num_leaves' : [int(x) for x in np.linspace(start = 20, stop = 3000, num = 10)],
+#                 'min_data_in_leaf' : [int(x) for x in np.linspace(start = 100, stop = 1000, num = 10)]
+#             }
+#         },
+#         'LogisticRegression': {
+#             'model': LogisticRegression(random_state=models_tuning_seed, max_iter=1000),
+#             'params': {
+#                 'penalty': ['l1', 'l2'],
+#                 'C' : [0.001, 0.01, 0.1, 1],
+#                 'solver': ['newton-cg', 'lbfgs', 'sag', 'saga'],
+#             }
+#         },
         'RandomForestClassifier': {
             'model': RandomForestClassifier(random_state=models_tuning_seed),
             'params': {
@@ -37,14 +37,14 @@ def get_folktables_employment_models_params_for_tuning(models_tuning_seed):
                 'bootstrap': [True, False]
             }
         },
-        'MLPClassifier': {
-            'model': MLPClassifier(hidden_layer_sizes=(100,100,), random_state=models_tuning_seed, max_iter=1000),
-            'params': {
-                'activation': ['logistic', 'tanh', 'relu'],
-                'solver': ['lbfgs', 'sgd', 'adam'],
-                'learning_rate': ['constant', 'invscaling', 'adaptive']
-            }
-        }
+#         'MLPClassifier': {
+#             'model': MLPClassifier(hidden_layer_sizes=(100,100,), random_state=models_tuning_seed, max_iter=1000),
+#             'params': {
+#                 'activation': ['logistic', 'tanh', 'relu'],
+#                 'solver': ['lbfgs', 'sgd', 'adam'],
+#                 'learning_rate': ['constant', 'invscaling', 'adaptive']
+#             }
+#         }
     }
 
 
