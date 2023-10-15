76a7c9e40f94f92f64558d10ad8a92071d78700f
denysgerasymuk799 <herasymuk@ucu.edu.ua>
2023-10-15 13:36:25 -0400

    Setup for new experiments

--- configs/models_config_for_tuning.py
+++ configs/models_config_for_tuning.py
@@ -27,7 +27,8 @@ def get_folktables_employment_models_params_for_tuning(models_tuning_seed):
             'model': RandomForestClassifier(random_state=models_tuning_seed),
             'params': {
                 # 'n_estimators': [100, 200, 500, 700, 1000],
-                'n_estimators': [100, 200, 500, 700],
+#                 'n_estimators': [50, 100, 200, 300, 400, 500],
+                'n_estimators': [50, 100, 200, 300, 400],
                 'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
                 'min_samples_split': [2, 5, 10],
                 'min_samples_leaf': [1, 2, 4],
