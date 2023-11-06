a042637adef6657279d3ea146f98e288967d8968
proc1v <nazarprotsiv02@gmail.com>
2023-11-06 23:29:04 +0200

    Added postprocessing experiment

--- source/experiment_interface.py
+++ source/experiment_interface.py
@@ -6,6 +6,8 @@ from datetime import datetime, timezone
 from sklearn.compose import ColumnTransformer
 from IPython.display import display
 
+from aif360.algorithms.postprocessing import EqOddsPostprocessing
+
 from virny.user_interfaces.metrics_computation_interfaces import compute_metrics_multiple_runs_with_db_writer, \
     compute_metrics_multiple_runs_with_multiple_test_sets
 from virny.utils.custom_initializers import create_models_config_from_tuned_params_df
@@ -344,4 +346,100 @@ def run_exp_iter_with_mult_set_and_preprocessing_intervention(data_loader, exper
                                                               db_writer_func=db_writer_func,
                                                               verbose=0)
 
+    logger.info("Experiment run was successful!")
+
+def run_exp_iter_with_eqq_odds_postprocessing(data_loader, experiment_seed, test_set_fraction, db_writer_func,
+                                       models_params_for_tuning,
+                                       metrics_computation_config, custom_table_fields_dct,
+                                       with_tuning: bool = False, save_results_dir_path: str = None,
+                                       tuned_params_df_path: str = None, num_folds_for_tuning: int = 3,
+                                       verbose: bool = False, dataset_name: str = 'CreditCardDefaultDataset'):
+    custom_table_fields_dct['dataset_split_seed'] = experiment_seed
+    custom_table_fields_dct['model_init_seed'] = experiment_seed
+    custom_table_fields_dct['fair_intervention_params_lst'] = str([1.0, 0.0])
+
+    logger = get_logger()
+    logger.info(f"Start an experiment iteration for the following custom params:")
+    pprint(custom_table_fields_dct)
+    print('\n', flush=True)
+
+    init_data_loader = copy.deepcopy(data_loader)
+    
+    if dataset_name == "CreditCardDefaultDataset":
+        data_loader.categorical_columns = [col for col in data_loader.categorical_columns if col not in ('sex')]
+        data_loader.X_data['sex_binary'] = data_loader.X_data['sex'].apply(lambda x: 1 if x == 'male' else 0)
+        data_loader.full_df = data_loader.full_df.drop(['sex'], axis=1)
+        data_loader.X_data = data_loader.X_data.drop(['sex'], axis=1)
+
+        # Preprocess the dataset using the defined preprocessor
+        column_transformer = get_simple_preprocessor(data_loader)
+        base_flow_dataset = preprocess_dataset(data_loader, column_transformer, test_set_fraction, experiment_seed)
+        base_flow_dataset.init_features_df = init_data_loader.full_df.drop(init_data_loader.target, axis=1, errors='ignore')
+        base_flow_dataset.X_train_val['sex_binary'] = data_loader.X_data.loc[base_flow_dataset.X_train_val.index, 'sex_binary']
+        base_flow_dataset.X_test['sex_binary'] = data_loader.X_data.loc[base_flow_dataset.X_test.index, 'sex_binary']
+
+        privileged_groups = [{'sex_binary': 1}]
+        unprivileged_groups = [{'sex_binary': 0}]
+    else:
+        raise ValueError('The dataset is not supported for this experiment')
+    
+    if verbose:
+        logger.info("The dataset is preprocessed")
+        print("Top indexes of an X_test in a base flow dataset: ", base_flow_dataset.X_test.index[:20])
+        print("Top indexes of an y_test in a base flow dataset: ", base_flow_dataset.y_test.index[:20])
+
+    # Tune model parameters if needed
+    if with_tuning:
+        cur_base_flow_dataset = copy.deepcopy(base_flow_dataset)
+        # Tune models and create a models config for metrics computation
+        tuned_params_df, models_config = tune_ML_models(models_params_for_tuning, cur_base_flow_dataset,
+                                                        metrics_computation_config.dataset_name,
+                                                        n_folds=num_folds_for_tuning)
+
+        # Create models_config from the saved tuned_params_df for higher reliability
+        date_time_str = datetime.now(timezone.utc).strftime("%Y%m%d__%H%M%S")
+        os.makedirs(save_results_dir_path, exist_ok=True)
+        tuned_df_path = os.path.join(save_results_dir_path,
+                                        f'tuning_results_{metrics_computation_config.dataset_name}_{date_time_str}.csv')
+        tuned_params_df.to_csv(tuned_df_path, sep=",", columns=tuned_params_df.columns, float_format="%.4f", index=False)
+        logger.info("Models are tuned and saved to a file")
+    else:
+        print('Path for tuned params: ', tuned_params_df_path)
+        models_config = create_models_config_from_tuned_params_df(models_params_for_tuning, tuned_params_df_path)
+        print(f'{list(models_config.keys())[0]}: ', models_config[list(models_config.keys())[0]].get_params())
+        logger.info("Models config is loaded from the input file")
+
+    postprocessor = EqOddsPostprocessing(privileged_groups=privileged_groups,
+                                         unprivileged_groups=unprivileged_groups,
+                                         seed=experiment_seed) # set 100 and different splits of 
+
+    # Compute metrrics with postprocessing
+    base_flow_dataset_with_postprocessing = copy.deepcopy(base_flow_dataset)
+    custom_table_fields_dct['intervention_param'] = 1.0
+    logger.info("Start computing metrics with postprocessing")
+    print("Model computation config without postprocessing: ", metrics_computation_config)
+    compute_metrics_multiple_runs_with_db_writer(dataset=base_flow_dataset_with_postprocessing,
+                                                 config=metrics_computation_config,
+                                                 models_config=models_config,
+                                                 custom_tbl_fields_dct=custom_table_fields_dct,
+                                                 db_writer_func=db_writer_func,
+                                                 postprocessor=postprocessor,
+                                                 postprocessing_sensitive_attribute="sex_binary",
+                                                 verbose=0)
+    
+    # Compute metrics without postprocessing
+    base_flow_dataset_without_postprocessing = copy.deepcopy(base_flow_dataset)
+    custom_table_fields_dct['intervention_param'] = 0.0
+    
+    postprocessing_metrics_computation_config = copy.deepcopy(metrics_computation_config)
+    postprocessing_metrics_computation_config.computation_mode = 'error_analysis'
+
+    logger.info("Start computing metrics without postprocessing")
+    print("Model computation config without postprocessing: ", postprocessing_metrics_computation_config)
+    compute_metrics_multiple_runs_with_db_writer(dataset=base_flow_dataset_without_postprocessing,
+                                                 config=postprocessing_metrics_computation_config,
+                                                 models_config=models_config,
+                                                 custom_tbl_fields_dct=custom_table_fields_dct,
+                                                 db_writer_func=db_writer_func,
+                                                verbose=0)
     logger.info("Experiment run was successful!")
