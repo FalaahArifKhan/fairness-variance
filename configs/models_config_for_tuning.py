9d300f0794ea8e7aae88af06297922439f04d8ac
denysgerasymuk799 <herasymuk@ucu.edu.ua>
2024-01-04 04:52:41 -0500

    Save tuned hyper-params for EqOdds Student Performance

--- configs/models_config_for_tuning.py
+++ configs/models_config_for_tuning.py
@@ -4,19 +4,19 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
 from xgboost import XGBClassifier
-# from lightgbm import LGBMClassifier
+from lightgbm import LGBMClassifier
 
 
 def get_folktables_employment_models_params_for_tuning(models_tuning_seed):
     return {
-        # 'LGBMClassifier': {
-        #     'model': LGBMClassifier(random_state=models_tuning_seed),
-        #     'params': {
-        #         'max_depth' : [i for i in range(3,12)],
-        #         'num_leaves' : [int(x) for x in np.linspace(start = 20, stop = 3000, num = 10)],
-        #         'min_data_in_leaf' : [int(x) for x in np.linspace(start = 100, stop = 1000, num = 10)]
-        #     }
-        # },
+        'LGBMClassifier': {
+            'model': LGBMClassifier(random_state=models_tuning_seed),
+            'params': {
+                'max_depth' : [i for i in range(3,12)],
+                'num_leaves' : [int(x) for x in np.linspace(start = 20, stop = 3000, num = 10)],
+                'min_data_in_leaf' : [int(x) for x in np.linspace(start = 100, stop = 1000, num = 10)]
+            }
+        },
         'LogisticRegression': {
             'model': LogisticRegression(random_state=models_tuning_seed, max_iter=1000),
             'params': {
@@ -25,18 +25,18 @@ def get_folktables_employment_models_params_for_tuning(models_tuning_seed):
                 'solver': ['newton-cg', 'lbfgs', 'sag', 'saga'],
             }
         },
-        # 'RandomForestClassifier': {
-        #     'model': RandomForestClassifier(random_state=models_tuning_seed),
-        #     'params': {
-        #         # 'n_estimators': [100, 200, 500, 700, 1000],
-        #         # 'n_estimators': [50, 100, 200, 300, 400],
-        #         'n_estimators': [100, 200, 500],
-        #         'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
-        #         'min_samples_split': [2, 5, 10],
-        #         'min_samples_leaf': [1, 2, 4],
-        #         'bootstrap': [True, False]
-        #     }
-        # },
+        'RandomForestClassifier': {
+            'model': RandomForestClassifier(random_state=models_tuning_seed),
+            'params': {
+                # 'n_estimators': [100, 200, 500, 700, 1000],
+                # 'n_estimators': [50, 100, 200, 300, 400],
+                'n_estimators': [100, 200, 500],
+                'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
+                'min_samples_split': [2, 5, 10],
+                'min_samples_leaf': [1, 2, 4],
+                'bootstrap': [True, False]
+            }
+        },
 #         'MLPClassifier': {
 #             'model': MLPClassifier(hidden_layer_sizes=(100,100,), random_state=models_tuning_seed, max_iter=1000),
 #             'params': {
