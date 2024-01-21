4176f851a4e8eb8480f1e1da372479705ff86da6
denysgerasymuk799 <herasymuk@ucu.edu.ua>
2024-01-21 15:51:03 +0200

    Cleaned configs, data, results, and source folders

--- configs/models_config_for_tuning.py
+++ configs/models_config_for_tuning.py
@@ -28,9 +28,9 @@ def get_folktables_employment_models_params_for_tuning(models_tuning_seed):
         'RandomForestClassifier': {
             'model': RandomForestClassifier(random_state=models_tuning_seed),
             'params': {
-                # 'n_estimators': [100, 200, 500, 700, 1000],
+                'n_estimators': [100, 200, 500, 700, 1000],
                 # 'n_estimators': [50, 100, 200, 300, 400],
-                'n_estimators': [100, 200, 500],
+                # 'n_estimators': [100, 200, 500],
                 'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
                 'min_samples_split': [2, 5, 10],
                 'min_samples_leaf': [1, 2, 4],
@@ -53,7 +53,8 @@ def get_model_params_for_mult_repair_levels(models_tuning_seed):
         'RandomForestClassifier': {
             'model': RandomForestClassifier(random_state=models_tuning_seed),
             'params': {
-                'n_estimators': [100, 200, 500],
+                'n_estimators': [100, 200, 500, 700, 1000],
+                # 'n_estimators': [100, 200, 500],
                 'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
                 'min_samples_split': [2, 5, 10],
                 'min_samples_leaf': [1, 2, 4],
@@ -74,44 +75,3 @@ def get_folktables_employment_models_params_for_tuning2(models_tuning_seed):
             }
         }
     }
-
-
-# def get_compas_models_params_for_tuning(models_tuning_seed):
-#     return {
-#         'DecisionTreeClassifier': {
-#             'model': DecisionTreeClassifier(random_state=models_tuning_seed),
-#             'params': {
-#                 "max_depth": [20, 30],
-#                 "min_samples_split" : [0.1],
-#                 "max_features": ['sqrt'],
-#                 "criterion": ["gini", "entropy"]
-#             }
-#         },
-#         'LogisticRegression': {
-#             'model': LogisticRegression(random_state=models_tuning_seed),
-#             'params': {
-#                 'penalty': ['l2'],
-#                 'C' : [0.0001, 0.1, 1, 100],
-#                 'solver': ['newton-cg', 'lbfgs'],
-#                 'max_iter': [250],
-#             }
-#         },
-#         'RandomForestClassifier': {
-#             'model': RandomForestClassifier(random_state=models_tuning_seed),
-#             'params': {
-#                 "max_depth": [6, 10],
-#                 "min_samples_leaf": [1],
-#                 "n_estimators": [50, 100],
-#                 "max_features": [0.6]
-#             }
-#         },
-#         'XGBClassifier': {
-#             'model': XGBClassifier(random_state=models_tuning_seed, verbosity=0),
-#             'params': {
-#                 'learning_rate': [0.1],
-#                 'n_estimators': [200],
-#                 'max_depth': [5, 7],
-#                 'lambda':  [10, 100]
-#             }
-#         }
-#     }
